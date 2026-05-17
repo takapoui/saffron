620592ba6cec1c4266b7501c972fee53641636fd
Reza Takapoui <8922427+takapoui@users.noreply.github.com>
2026-05-17 15:00:59 -0500

    Dedup some functions in two trainers

--- src/saffron/model/__init__.py
+++ src/saffron/model/__init__.py
@@ -3,4 +3,17 @@ from .config import BaseConfig, GPT2Config, HFConfig
 from .gpt2 import GPT2
 from .hf import HFModel
 
-__all__ = ["BaseConfig", "BaseModel", "GPT2", "GPT2Config", "HFConfig", "HFModel"]
+MODEL_REGISTRY: dict[str, type[BaseModel]] = {
+    "gpt2": GPT2,
+    "hf": HFModel,
+}
+
+__all__ = [
+    "MODEL_REGISTRY",
+    "BaseConfig",
+    "BaseModel",
+    "GPT2",
+    "GPT2Config",
+    "HFConfig",
+    "HFModel",
+]
